4db27e4e33cd22621f0067ba45789187ad0b4b86
Goldlender <129844492+Goldlender@users.noreply.github.com>
2024-09-26 14:15:12 +0800

    icons

--- GUI exercises/menu page.py
+++ GUI exercises/menu page.py
@@ -10,11 +10,38 @@ root.title('Menu')
 #root.withdraw()
 
 #icons 
-image = Image.open('toggle2 icon.png').resize((30, 30))
+image = Image.open('toggle.png').resize((30, 30))
 toggle_icon = ImageTk.PhotoImage(image)
-menu_bar = Frame(root, bg='#384B70')
-toggle_button = Button(menu_bar, image=toggle_icon, bg='#384B70')
+
+image2 = Image.open('home.png').resize((30, 30))
+home_icon = ImageTk.PhotoImage(image2)
+
+image3 = Image.open('methods.png').resize((30, 30))
+methods_icon = ImageTk.PhotoImage(image3)
+
+image4 = Image.open('info.png').resize((30, 30))
+info_icon = ImageTk.PhotoImage(image4)
+
+image5 = Image.open('account.png').resize((30, 30))
+account_icon = ImageTk.PhotoImage(image5)
+
+#widgets/buttons
+menu_bar = Frame(root, bg='#5271ff')
+toggle_button = Button(menu_bar, image=toggle_icon, bg='#5271ff', bd=0, activebackground='#5271ff')
 toggle_button.place(x=4, y=10)
+
+home_button = Button(menu_bar, image=home_icon, bg='#5271ff', bd=0, activebackground='#5271ff')
+home_button.place(x=6, y=100) 
+
+methods_button = Button(menu_bar, image=methods_icon, bg='#5271ff', bd=0, activebackground='#5271ff')
+methods_button.place(x=6, y=150)
+
+info_button = Button(menu_bar, image=info_icon, bg='#5271ff', bd=0, activebackground='#5271ff')
+info_button.place(x=6, y=200)
+
+account_button = Button(menu_bar, image=account_icon, bg='#5271ff', bd=0, activebackground='#5271ff')
+account_button.place(x=6, y=250)
+
 menu_bar.pack(side=LEFT, fill=Y, padx=3, pady=4)
 menu_bar.pack_propagate(flag=False)
 
