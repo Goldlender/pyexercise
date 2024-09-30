f2de15d0c1f5d81cd89ec9da5798dc78e6f2d8ff
Goldlender <129844492+Goldlender@users.noreply.github.com>
2024-09-30 15:05:20 +0800

    indentation changes

--- combined App.py
+++ combined App.py
@@ -10,6 +10,28 @@ root.title('Sign In')
 root.geometry('400x400')
 root.configure(bg='#B7B7B7')
 
+image = Image.open('toggle.png').resize((30, 30))
+toggle_icon = ImageTk.PhotoImage(image)
+
+image2 = Image.open('home.png').resize((30, 30))
+home_icon = ImageTk.PhotoImage(image2)
+
+image3 = Image.open('methods.png').resize((30, 30))
+methods_icon = ImageTk.PhotoImage(image3)
+
+image4 = Image.open('info.png').resize((30, 30))
+info_icon = ImageTk.PhotoImage(image4)
+
+image5 = Image.open('account.png').resize((30, 30))
+account_icon = ImageTk.PhotoImage(image5)
+
+image6 = Image.open('close.png').resize((30, 30))
+close_icon = ImageTk.PhotoImage(image6)
+
+image7 = Image.open('Wallpaper2.png').resize((590, 590))
+wallp = ImageTk.PhotoImage(image7)
+
+
 class Sign_in:
         def __init__(self, master):
                 self.myframe = Frame(master, bg='#B7B7B7')
@@ -87,70 +109,117 @@ class Sign_in:
 
                 self.myframe.pack()
 
+
 class Menu:
      def __init__(self, master):
-          image = Image.open('toggle.png').resize((30, 30))
-          toggle_icon = ImageTk.PhotoImage(image)
+          self.master = root
+          self.master.geometry('600x600')
+          self.master.title('Menu')
 
-          image2 = Image.open('home.png').resize((30, 30))
-          home_icon = ImageTk.PhotoImage(image2)
+               #widgets/buttons
+          menu_bar = Frame(root, bg='#5271ff')
+          toggle_button = Button(menu_bar, image=toggle_icon, bg='#5271ff', bd=0, 
+                                activebackground='#5271ff', command=self.extend_menu)
+          toggle_button.place(x=4, y=10)
 
-          image3 = Image.open('methods.png').resize((30, 30))
-          methods_icon = ImageTk.PhotoImage(image3)
+            #home icon
+          home_button = Button(menu_bar, image=home_icon, bg='#5271ff', 
+                                bd=0, activebackground='#5271ff', command=lambda: self.indicator(indi_lbl=home_ind, page_switch=self.home))
+          home_button.place(x=6, y=100, height=40, width=30) 
 
-          image4 = Image.open('info.png').resize((30, 30))
-          info_icon = ImageTk.PhotoImage(image4)
+          home_ind = Label(menu_bar, bg='white')
+          home_ind.place(x=3, y=100, height=40, width=3)
 
-          image5 = Image.open('account.png').resize((30, 30))
-          account_icon = ImageTk.PhotoImage(image5)
+          home_label = Label(menu_bar, text='Home', bg='#5271ff', fg='white',
+                            font=('Arial', 15), anchor=W)
+          home_label.place(x=45, y=100, width=100, height=40)
+          home_label.bind('<Button-1>', lambda e: self.indicator(indi_lbl=home_ind, page_switch=self.home ))
 
-          image6 = Image.open('close.png').resize((30, 30))
-          close_icon = ImageTk.PhotoImage(image6)
+            #methods icon
+          methods_button = Button(menu_bar, image=methods_icon, bg='#5271ff', 
+                                    bd=0, activebackground='#5271ff', command=lambda: self.indicator(indi_lbl=method_ind, page_switch=self.methods))
+          methods_button.place(x=6, y=170, height=40, width=30)
 
-          image7 = Image.open('Wallpaper2.png').resize((590, 590))
-          wallp = ImageTk.PhotoImage(image7)
+          method_ind = Label(menu_bar, bg='#5271ff')
+          method_ind.place(x=3, y=170, height=40, width=3)
 
-     def indicator(indi_lbl, page_switch):
-          home_ind.config(bg='#5271ff')
-          info_ind.config(bg='#5271ff')
-          method_ind.config(bg='#5271ff')
-          account_ind.config(bg='#5271ff')
+          method_label = Label(menu_bar, text='Models', bg='#5271ff', fg='white',
+                        font=('Arial', 15), anchor=W)
+          method_label.place(x=45, y=170, width=120, height=40)
+          method_label.bind('<Button-1>', lambda e: self.indicator(indi_lbl=method_ind, page_switch=self.methods))
+
+            #info_icon
+          info_button = Button(menu_bar, image=info_icon, bg='#5271ff',
+                                bd=0, activebackground='#5271ff', command=lambda: self.indicator(indi_lbl=info_ind, page_switch=self.info))
+          info_button.place(x=6, y=240, height=40, width=30)
+
+          info_ind = Label(menu_bar, bg='#5271ff')
+          info_ind.place(x=3, y=240, height=40, width=3)
+
+          info_label = Label(menu_bar, text='Info', bg='#5271ff', fg='white',
+                        font=('Arial', 15), anchor=W)
+          info_label.place(x=45, y=240, width=100, height=40)
+          info_label.bind('<Button-1>', lambda e: self.indicator(indi_lbl=info_ind, page_switch=self.info))
+
+            #account_icon
+          account_button = Button(menu_bar, image=account_icon, bg='#5271ff', bd=0, 
+                            activebackground='#5271ff', command=lambda: self.indicator(indi_lbl=account_ind))
+          account_button.place(x=6, y=310, height=40, width=30)
+
+          account_ind = Label(menu_bar, bg='#5271ff')
+          account_ind.place(x=3, y=310, height=40, width=3)
+
+          account_label = Label(menu_bar, text='Account', bg='#5271ff', fg='white',
+                        font=('Arial', 15), anchor=W)
+          account_label.place(x=45, y=310, width=120, height=40)
+          account_label.bind('<Button-1>', lambda e: self.indicator(indi_lbl=account_ind))
+
+          menu_bar.pack(side=LEFT, fill=Y, padx=3, pady=4)
+          menu_bar.pack_propagate(flag=False)
+
+          menu_bar.configure(width=45)
+
+     def indicator(self, indi_lbl, page_switch):
+          self.home_ind.config(bg='#5271ff')
+          self.info_ind.config(bg='#5271ff')
+          self.method_ind.config(bg='#5271ff')
+          self.account_ind.config(bg='#5271ff')
 
           indi_lbl.config(bg='white')
-          if menu_bar.winfo_width() > 45:
-                fold_menu()
+          if self.menu_bar.winfo_width() > 45:
+                self.fold_menu()
 
           for frame in page.winfo_children():
                frame.destroy()
                page_switch()
 
-     def animate_menu():
-           current_width = menu_bar.winfo_width()
+     def animate_menu(self):
+           self.current_width = self.menu_bar.winfo_width()
            if not current_width > 200:
                 current_width += 10
-                menu_bar.config(width=current_width)
+                self.menu_bar.config(width=current_width)
                 
-                root.after(ms=8, func=animate_menu)
+                root.after(ms=8, func=self.animate_menu)
 
-     def extend_menu():
-           animate_menu()
-           toggle_button.config(image=close_icon)
-           toggle_button.config(command=fold_menu)
+     def extend_menu(self):
+           self.animate_menu()
+           self.toggle_button.config(image=close_icon)
+           self.toggle_button.config(command=self.fold_menu)
 
-     def animate_folding():
-           current_width = menu_bar.winfo_width()
+     def animate_folding(self):
+           current_width = self.menu_bar.winfo_width()
            if current_width != 45:
                  current_width -= 10
-                 menu_bar.config(width=current_width)
+                 self.menu_bar.config(width=current_width)
                  
-                 root.after(ms=8, func=animate_folding)
+                 root.after(ms=8, func=self.animate_folding)
 
-     def fold_menu():
-           animate_folding()
-           toggle_button.config(image=toggle_icon)
-           toggle_button.config(command=extend_menu)
+     def fold_menu(self):
+           self.animate_folding()
+           self.toggle_button.config(image=toggle_icon)
+           self.toggle_button.config(command=self.extend_menu)
 
-     def home():
+     def home(self):
            h_page=Frame(page)
            lb = Label(h_page, image=wallp)
            lb.pack()
@@ -158,7 +227,7 @@ class Menu:
            h_page.pack(fill=BOTH, expand=True)
 
     
-     def methods():
+     def methods(self):
       #labels
       m_page=Frame(page)
       lb = Label(m_page, text='SELECT MODELS', font=('Times new Roman', 20, 'bold'))
@@ -174,7 +243,7 @@ class Menu:
 
       m_page.pack(fill=BOTH, expand=True)
 
-     def info():
+     def info(self):
       i_page=Frame(page)
       lb = Label(i_page, text='This is a Rock Fragmentation App \nMade by Central South University foreign student', font=('Times new Roman', 12,))
       lb.place(x=100, y=200)
@@ -187,70 +256,6 @@ class Menu:
     #  home()
     #  methods()
     #  info()
-
-     #widgets/buttons
-     menu_bar = Frame(root, bg='#5271ff')
-     toggle_button = Button(menu_bar, image=toggle_icon, bg='#5271ff', bd=0, 
-                            activebackground='#5271ff', command=extend_menu)
-     toggle_button.place(x=4, y=10)
-
-     #home icon
-     home_button = Button(menu_bar, image=home_icon, bg='#5271ff', 
-                          bd=0, activebackground='#5271ff', command=lambda: indicator(indi_lbl=home_ind, page_switch=home))
-     home_button.place(x=6, y=100, height=40, width=30) 
-
-     home_ind = Label(menu_bar, bg='white')
-     home_ind.place(x=3, y=100, height=40, width=3)
-
-     home_label = Label(menu_bar, text='Home', bg='#5271ff', fg='white',
-                        font=('Arial', 15), anchor=W)
-     home_label.place(x=45, y=100, width=100, height=40)
-     home_label.bind('<Button-1>', lambda e: indicator(indi_lbl=home_ind, page_switch=home ))
-
-     #methods icon
-     methods_button = Button(menu_bar, image=methods_icon, bg='#5271ff', 
-                             bd=0, activebackground='#5271ff', command=lambda: indicator(indi_lbl=method_ind, page_switch=methods))
-     methods_button.place(x=6, y=170, height=40, width=30)
-
-     method_ind = Label(menu_bar, bg='#5271ff')
-     method_ind.place(x=3, y=170, height=40, width=3)
-
-     method_label = Label(menu_bar, text='Models', bg='#5271ff', fg='white',
-                   font=('Arial', 15), anchor=W)
-     method_label.place(x=45, y=170, width=120, height=40)
-     method_label.bind('<Button-1>', lambda e: indicator(indi_lbl=method_ind, page_switch=methods))
-
-     #info_icon
-     info_button = Button(menu_bar, image=info_icon, bg='#5271ff',
-                          bd=0, activebackground='#5271ff', command=lambda: indicator(indi_lbl=info_ind, page_switch=info))
-     info_button.place(x=6, y=240, height=40, width=30)
-
-     info_ind = Label(menu_bar, bg='#5271ff')
-     info_ind.place(x=3, y=240, height=40, width=3)
-
-     info_label = Label(menu_bar, text='Info', bg='#5271ff', fg='white',
-                   font=('Arial', 15), anchor=W)
-     info_label.place(x=45, y=240, width=100, height=40)
-     info_label.bind('<Button-1>', lambda e: indicator(indi_lbl=info_ind, page_switch=info))
-
-     #account_icon
-     account_button = Button(menu_bar, image=account_icon, bg='#5271ff', bd=0, 
-                        activebackground='#5271ff', command=lambda: indicator(indi_lbl=account_ind))
-     account_button.place(x=6, y=310, height=40, width=30)
-
-     account_ind = Label(menu_bar, bg='#5271ff')
-     account_ind.place(x=3, y=310, height=40, width=3)
-
-     account_label = Label(menu_bar, text='Account', bg='#5271ff', fg='white',
-                   font=('Arial', 15), anchor=W)
-     account_label.place(x=45, y=310, width=120, height=40)
-     account_label.bind('<Button-1>', lambda e: indicator(indi_lbl=account_ind))
-
-     menu_bar.pack(side=LEFT, fill=Y, padx=3, pady=4)
-     menu_bar.pack_propagate(flag=False)
-
-     menu_bar.configure(width=45)
-
 
 page = Sign_in(root)
 
